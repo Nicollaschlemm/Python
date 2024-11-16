795c3764d6733a2e293bc3e70f26f663a7758efe
nicollas <nicollas_pires@hotmail.com>
2024-11-16 20:05:12 -0300

    Adcionando novo exercicio

--- Basico/loop_while/Exercicio_3.py
+++
@@ -1,3 +0,0 @@
-#Escreva um programa que mostre na tela os 10 primeiros termos de uma progressão aritmética (PA) 
-#com primeiro termo P e razão R. Os dois números P e R são inteiros e devem ser lidos do teclad
-int =1
